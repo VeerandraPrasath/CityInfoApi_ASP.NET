552c173fb4673b5ec0a55ec5cd42d5b81c23ade6
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-05-19 19:57:33 +0530

    Set route for the CityController

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -3,9 +3,10 @@
 namespace CityInfoApi.Controllers
 {
     [ApiController]
+    [Route("api/cities")]
     public class CityController:ControllerBase
     {
-        [HttpGet("api/cities")]
+        [HttpGet()]
         public JsonResult GetCities()
         {
             return new JsonResult(new List<object>
