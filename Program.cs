ceae454f840a71770f398c5dcd1aeacaa2d1f8ef
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-05-20 12:10:00 +0530

    Included the acceptable formatters

--- Program.cs
+++ Program.cs
@@ -7,9 +7,28 @@ namespace CityInfoApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services to the container
+            //builder.Services.AddControllers();
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(options =>
+            {
+                options.ReturnHttpNotAcceptable = true; //If the client does not accept the response type, return 406 Not Acceptable.If will not accept the request of the client because the response type will not support the client.
+            }).AddXmlDataContractSerializerFormatters(); //Here we are specifying that we can send the reponse in xml format,so it accept the request if the requested format is xml.
+
+            //Add some extra details to the response .So that the client can get more details about the response
+            //builder.Services.AddProblemDetails(options =>
+            //{
+
+            //    options.CustomizeProblemDetails = ctx =>
+            //    {
+            //        ctx.ProblemDetails.Extensions.Add("additionalInfo", "Additional info example");
+            //        ctx.ProblemDetails.Extensions.Add("server", Environment.MachineName);
+            //    };
+            //});
+
+
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
