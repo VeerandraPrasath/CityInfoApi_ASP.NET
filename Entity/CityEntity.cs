426f07cb9183c683071c7a0b1bf7f16b60db1320
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-05-21 11:40:12 +0530

    Created Entities

---
+++ Entity/CityEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfoApi.Entity
+{
+    public class CityEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]    
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [MaxLength(50, ErrorMessage = "Name cannot be more than 100 characters.")]
+        public string Name { get; set; }
+
+
+        [MaxLength(100, ErrorMessage = "Description cannot be more than 100 characters.")]
+        public string Description { get; set; }
+
+
+        public ICollection<PointOfInterestEntity> PointOfInterestEntities { get; set; } = new List<PointOfInterestEntity>();//Also act as Navigational Property
+    }
+}
---
+++ Entity/PointOfInterestEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfoApi.Entity
+{
+    public class PointOfInterestEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [MaxLength(50, ErrorMessage = "Name cannot be more than 100 characters.")]
+        public string Name { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Description cannot be more than 100 characters.")]
+        public string Description { get; set; }
+
+        [ForeignKey("CityId")]
+        public int CityId { get; set; }
+
+        public CityEntity City { get; set; }  //Naviagtion Property
+    }
+}
